784c99443b19e8f0a81b385bb75bcb87c08f73e9
EarthWormGym <adam56.beresford@gmail.com>
2023-01-12 20:24:04 +0000

    Form refinements

--- src/RecipeForm.js
+++ src/RecipeForm.js
@@ -5,66 +5,74 @@ const RecipeForm = () => {
 
     const [state, setState] = useState({
         recipeName: "",
-        picture: "",
         ingrediants: "",
+        picture: "",
         type: ""
-    })
+    });
 
     const handleChange = (event) => {
         const value = event.target.value;
         setState({
             ...state,
             [event.target.recipeName]: value,
-            [event.target.picture]: value,
             [event.target.ingrediants]: value,
+            [event.target.picture]: value,
             [event.target.type]: value
         });
-    }
+    };
     
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log(state);
-    }
+    };
 
     return (
         <form onSubmit={handleSubmit}>
-            <label>
-                Recipe Name:
-                <input 
-                    type="text" 
-                    name="recipeName"
-                    value={state.name}
-                    onChange={handleChange}
-                />
-            </label>
-            <label>
-                Recipe Picture:
-                <input 
-                    type="image" 
-                    name="recipeImage"
-                    value={state.picture}
-                    onChange={handleChange}
-                />
-            </label>
-            <label>
-                Ingrediants:
-                <input 
-                    type="text" 
-                    name="recipeIngrediants"
-                    value={state.ingrediants}
-                    onChange={handleChange}
-                />
-            </label>
-            <label>
-                Recipe Type:
-                <select name="recipeType" value={state.type} onChange={handleChange} placeholder="Select...">
-                    <option value="asian">Asian</option>
-                    <option value="japanese">Japanese</option>
-                    <option value="indian">Indian</option>
-                    <option value="english">English</option>
-                    <option value="african">African</option>
-                </select>
-            </label>
+            <div>
+                <label>
+                    Recipe Name:
+                    <input 
+                        type="text" 
+                        name="recipeName"
+                        value={state.recipeName.value}
+                        onChange={handleChange}
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Ingrediants:
+                    <input 
+                        type="text" 
+                        name="recipeIngrediants"
+                        value={state.ingrediants.value}
+                        onChange={handleChange}
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Recipe Picture:
+                    <input 
+                        type="image" 
+                        name="recipeImage"
+                        value={state.picture.value}
+                        onChange={handleChange}
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Recipe Type:
+                    <select name="recipeType" defaultValue="Select..." value={state.type.value} onChange={handleChange}>
+                        <option value="african">African</option>
+                        <option value="asian">Asian</option>
+                        <option value="indian">Indian</option>
+                        <option value="italian">Italian</option>
+                        <option value="japanese">Japanese</option>
+                    </select>
+                </label>
+            </div>
             <input type="submit" value="Submit" />
         </form>
     );
